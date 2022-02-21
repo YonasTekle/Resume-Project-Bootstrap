ab371c425ca5f755dab40be190aec48f6d962539
Yonas Tekle <yonas.t.tekle@outlook.com>
2022-02-21 19:08:10 +0000

    Added Google Maps with Markers

---
+++ assets/JS/maps.js
@@ -0,0 +1,26 @@
+function initMap() {
+    var map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 3,
+        center: {
+            lat: 46.619261,
+            lng: -33.134766
+        }
+    });
+
+    var labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+    var locations = [
+        { lat: 59.3294, lng: 18.0686 },
+        { lat: 60.0294, lng: 18.0686 },
+        { lat: 60.3294, lng: 18.7686 }
+    ];
+
+    var markers = locations.map(function(location, i) {
+        return new google.maps.Marker({
+            position: location,
+            label: labels[i % labels.length]
+        });
+    });
+
+    var markerCluster = new MarkerClusterer(map, markers, { imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m' });
+}
